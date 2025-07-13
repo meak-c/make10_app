7fcdbc63cb809edd2e15c66ded716d3d4c246715
meak-c <chithonmeak@gmail.com>
2025-07-13 16:04:12 +0900

    Fix: bind to 0.0.0.0 and use PORT env

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 from solver import solve
 
@@ -30,5 +32,5 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
